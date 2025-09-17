43bcf866312423a07e4df1e48a8cc83b937f941c
Andreasanchezgzz <23308051220760@cbtis122.edu.mx>
2025-09-16 18:21:37 -0600

    Update exercise7.js

--- Leccion 03 Objects/Ejercicio 07/exercise7.js
+++ Leccion 03 Objects/Ejercicio 07/exercise7.js
@@ -1,15 +1,15 @@
 // Ejercicio 7: Encontrar al participante con el mayor puntaje
 
 function encontrarMayorPuntaje(tablero) {
-  let maxNombre = "";
-  let maxPuntos = -Infinity; 
+    let mayor = null;      
+    let mayorPuntos = 0;    
 
-  for (let participante in tablero) {
-    if (tablero[participante] > maxPuntos) {
-      maxPuntos = tablero[participante];
-      maxNombre = participante;
+    for (let participante in tablero) {
+        if (tablero[participante] > mayorPuntos) {
+            mayor = participante;               
+            mayorPuntos = tablero[participante]; 
+        }
     }
-  }
 
-  return maxNombre;
+    return mayor; 
 }
