5e8065273ef474f715fb12ca22bd0d426a685f9c
Andreasanchezgzz <23308051220760@cbtis122.edu.mx>
2025-09-16 18:22:31 -0600

    Update exercise.js

--- Leccion 03 Objects/Ejercicio 08/exercise.js
+++ Leccion 03 Objects/Ejercicio 08/exercise.js
@@ -1,5 +1,12 @@
 // Ejercicio 8: Calcular el puntaje total de todos los participantes
 
 function calcularPuntajeTotal(tablero) {
-    
+    let total = 0; 
+
+  
+    for (let participante in tablero) {
+        total += tablero[participante]; 
+    }
+
+    return total; 
 }
