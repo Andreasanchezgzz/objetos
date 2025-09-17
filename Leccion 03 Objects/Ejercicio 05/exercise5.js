5feeb38cddad98a101ec461443a463df3ddf1b52
Andreasanchezgzz <23308051220760@cbtis122.edu.mx>
2025-09-16 18:16:14 -0600

    Update exercise5.js

--- Leccion 03 Objects/Ejercicio 05/exercise5.js
+++ Leccion 03 Objects/Ejercicio 05/exercise5.js
@@ -1,5 +1,7 @@
 // Ejercicio 5: Multiplicar los puntos de un participante
 
 function multiplicarPuntaje(tablero, nombre, factor) {
-
+  tablero[nombre] = tablero[nombre] * factor;
+  return tablero;
 }
+
