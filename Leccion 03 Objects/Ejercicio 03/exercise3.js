be63ddd7f4de5e531470c004f947c0c1c9558a5f
Andreasanchezgzz <23308051220760@cbtis122.edu.mx>
2025-09-16 18:14:18 -0600

    Update exercise3.js

--- Leccion 03 Objects/Ejercicio 03/exercise3.js
+++ Leccion 03 Objects/Ejercicio 03/exercise3.js
@@ -1,6 +1,8 @@
 // Ejercicio 3: Remover a un participante del tablero
 
 function removerParticipante(tablero, nombre) {
-
+  // Usamos delete para eliminar la propiedad con la clave "nombre"
+  delete tablero[nombre];
+  return tablero;
 }
 
